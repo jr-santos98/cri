570b964fb0b3d86f3b5a71b4c4c4c10c11c9910d
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-07-08 18:55:04 -0300

    Update release.sh

--- hack/release.sh
+++ hack/release.sh
@@ -43,17 +43,13 @@ OFFICIAL_RELEASE=${OFFICIAL_RELEASE:-false}
 # LOCAL_RELEASE indicates that containerd has been built and released
 # locally.
 LOCAL_RELEASE=${LOCAL_RELEASE:-false}
-if [ -z "${GOOS}" ]
+if [ -z "$GOOS" ]
 then
     GOOS=$(go env GOOS)
-else
-    echo $GOOS
 fi
-if [ -z "${GOARCH}" ]
+if [ -z "$GOARCH" ]
 then
     GOARCH=$(go env GOARCH)
-else
-    echo $GOARCH
 fi
 
 
